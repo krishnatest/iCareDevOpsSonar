3d71f0cb01e7f2a9efb7377c8652a1561e3b392c
kkumar92 <kkumar92@csc.com>
2016-06-13 14:48:45 +0530

    Whole Project Committed Date: 13-06-2016

--- iCare/WebContent/iCare.jsp
+++ iCare/WebContent/iCare.jsp
@@ -1,12 +1,65 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
+<meta charset="utf-8" />
+<title>::: Patient Portal - Home :::</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link rel="stylesheet" href="css/style.css" type="text/css" media="screen, projection" />
+<link rel="stylesheet" href="css/style_ie.css" type="text/css" media="screen, projection" />
+<!-- <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script> -->
 </head>
 <body>
-	<h1>Hello World </h1>
+	<div id="wrapper" class="LoginBackground">
+		<div id="headerlogin">
+			<div id="loginHearBg"></div>
+		</div>
+		<div id="logincontent">
+			<div id="mydiv">
+				<div class="row">
+					<div id="loginlogo">
+						<img src="images/logo.png">
+					</div>
+					<div id="logininput">
+						<form action="UserAuthentication" method="post" name="iCare" id="iCare">
+							<div id="logininputcontent">
+								<div style="color: #c80000" value="">${errorMessage }</div>
+								<span class="signin"> Sign In </span><br /> 
+								<span class="username"> Username </span><br /> 
+									<input type="text" class="textbox" name="userName" id="userName" ><br /> 
+								<span class="username"> Password </span><br /> 
+									<input style="margin-bottom: 10px;" type="password" class="textbox" name="password" id="password" ><br />
+								
+								<!-- Captcha Field added .......................... Start Here -->
+								
+ 
+								<span class="username"> Captcha 2 + 2 =</span> 
+									<input style="width: 85px;" type="text" class="textbox" name="captcha" id="captcha" >
+									<br />
+
+									
+								<!-- Captcha Field added .......................... End Here -->
+								
+								<span>
+									<input type="submit" name="submit" id="submit" value="Sign In" class="Signbtn" />
+								</span><br /> 
+								<span> <a href="#" class="mainlink">Unable to access my account</a></span><br /> 
+								<span> <a href="#" class="mainlink">Click here to Register </a></span>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- #content-->
+	</div>
+	<div id="loginfooterbg">
+		<div id="poweredby">
+			<p class="loginpoweredby">
+				Powered by :
+			</p>
+		</div>
+	</div>
 </body>
 </html>
---
+++ iCare/src/UserAuthentication.java
@@ -0,0 +1,104 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+//import org.apache.tomcat.util.buf.Base64;
+
+
+/**
+ * Servlet implementation class ServerValidation
+ */
+@WebServlet("/UserAuthentication")
+public class UserAuthentication extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	private String uName = "test";
+	private String uPwd = "test";
+
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Long captcha = null;
+		boolean resultC = true;
+
+		String userName = request.getParameter("userName");
+		String password = request.getParameter("password");
+		String captchaStr = request.getParameter("captcha");
+		
+		try {
+			if(!captchaStr.equals("")) {
+				captcha = Long.parseLong(captchaStr);
+			}
+        } catch (NumberFormatException e) {
+        	resultC = false;
+        	System.out.println("IndexOutOfBoundsException: " + e.getMessage());
+        }
+		
+
+		String error = "";
+
+		boolean result = true;
+
+		//AppWorks Logical Error Introduce
+		//if (userName == "") {
+		if (userName.equals("")) {
+			error += "Please enter User Name.<br/>";
+			result = false;
+		}  
+		
+		
+		if (password.equals("")) {
+			error += "Please enter Password.<br/>";
+			result = false;
+		}
+		
+		if (captcha == null || resultC == false || captcha != 4) {
+			error += "Please enter correct Captcha Value.<br/>";
+			result = false;
+		} else {
+			if (userName.equals(uName) && password.equals(uPwd) && captcha == 4) {
+				// Create a session object if it is already not  created.
+				HttpSession session = request.getSession(true);
+				session.setAttribute("sessUname", uName);
+				session.setAttribute("sessUpwd", uPwd);
+				
+				//System.out.println(session.getAttribute("sessUname"));
+				//System.out.println(session.getAttribute("sessUpwd"));
+				result = true;
+			} else {
+				error += "User Name or Password is incorrect.<br/>";
+				result = false;
+			}
+		}
+		
+		
+		if (result) {
+			response.sendRedirect("home.jsp");
+		} else {
+			request.setAttribute("errorMessage", error);
+			request.getRequestDispatcher("/iCare.jsp").forward(request, response);
+		}
+	}	
+	
+
+}
