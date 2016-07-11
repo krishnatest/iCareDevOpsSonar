12dfd0f06ed1327e614b8470ad534160ffa75c9c
kkumar92 <kkumar92@csc.com>
2016-07-11 15:05:27 +0530

    comitted for deployment

--- iCare/TestCases/DevTestIcare.java
+++ iCare/TestCases/DevTestIcare.java
@@ -27,8 +27,7 @@ public class DevTest extends TestCase {
 
 		public void setUp() throws Exception {
 			driver = new HtmlUnitDriver();
-			//baseUrl = "http://devops-devenv.ap-southeast-1.elasticbeanstalk.com/";
-			baseUrl = "http://localhost:6060/iCare/home.jsp";
+			baseUrl = "http://devopsicare-dev.ap-southeast-1.elasticbeanstalk.com/";
 			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 			driver.manage().window().maximize();
 		}
