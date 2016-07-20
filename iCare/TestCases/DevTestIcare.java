24950df7edfef1d7a6d631c1f2a48f9d2ccf0079
krishna <kkumar92@csc.com>
2016-07-20 18:02:44 +0530

    selenium test check

--- iCare/TestCases/DevTestIcare.java
+++ iCare/TestCases/DevTestIcare.java
@@ -33,7 +33,7 @@ public class DevTestIcare extends TestCase {
 
 		public void setUp() throws Exception {
 			driver = new HtmlUnitDriver();
-			baseUrl = "http://devopsicare-env.ap-southeast-1.elasticbeanstalk.com/home.jsp?name=test";
+			baseUrl = "http://devopsicare-dev.ap-southeast-1.elasticbeanstalk.com/home.jsp?name=test";
 			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 			driver.manage().window().maximize();
 		}
